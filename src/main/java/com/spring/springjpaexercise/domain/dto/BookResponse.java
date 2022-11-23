bb052427fc89f72a63352664a5d20d82e1bfecc1
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-23 13:18:01 +0900

    Book Response DTO(bookId,bookName,authorName)

---
+++ src/main/java/com/spring/springjpaexercise/domain/dto/BookResponse.java
@@ -0,0 +1,16 @@
+package com.spring.springjpaexercise.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookResponse {
+    private Long bookId;
+    private String bookName;
+    private String authorName;
+}
