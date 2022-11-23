53ebf9b08d60c32c90391c63dde28cf2455f36f0
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-23 13:18:25 +0900

    Book Service : author id와 이름 매칭

---
+++ src/main/java/com/spring/springjpaexercise/service/BookService.java
@@ -0,0 +1,36 @@
+package com.spring.springjpaexercise.service;
+
+import com.spring.springjpaexercise.domain.dto.BookResponse;
+import com.spring.springjpaexercise.domain.entity.Author;
+import com.spring.springjpaexercise.domain.entity.Book;
+import com.spring.springjpaexercise.repository.AuthorRepository;
+import com.spring.springjpaexercise.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+
+    public BookService(BookRepository bookRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    public List<BookResponse> find() {
+        List<Book> books = bookRepository.findAll();
+        List<BookResponse> bookResponseList= new ArrayList<>();
+        for(Book book : books){
+            Long authorId = book.getAuthorId();
+            Optional<Author> optionalAuthor = authorRepository.findById(authorId);
+            String authorName = optionalAuthor.get().getName();
+            bookResponseList.add(new BookResponse(book.getId(),book.getName(),authorName));
+        }
+        return bookResponseList;
+    }
+}
