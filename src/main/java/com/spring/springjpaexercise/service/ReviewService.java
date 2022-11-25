d8f6488e6077643a06b4d62f19be6a290f5e720a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-25 13:15:53 +0900

    Review Service: review id로 get하는 메소드 구현

---
+++ src/main/java/com/spring/springjpaexercise/service/ReviewService.java
@@ -0,0 +1,20 @@
+가package com.spring.springjpaexercise.service;
+
+import com.spring.springjpaexercise.domain.dto.ReviewResponse;
+import com.spring.springjpaexercise.domain.entity.Review;
+import com.spring.springjpaexercise.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public ReviewResponse find(Long id){
+        Review review = reviewRepository.findById(id).orElseThrow(()-> new RuntimeException("id에 해당하는 리뷰가 없습니다."));
+        return new ReviewResponse(review.getHospital().getName(),review.getTitle(), review.getContent(), review.getPatientName());
+    }
+}
