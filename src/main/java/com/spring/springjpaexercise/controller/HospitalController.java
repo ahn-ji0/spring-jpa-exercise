aab72527d87bc1255e979a81e94b2d19de2966e0
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-25 11:02:58 +0900

    Add: 병원 리뷰 보는 get 메소드 추가

--- src/main/java/com/spring/springjpaexercise/controller/HospitalController.java
+++ src/main/java/com/spring/springjpaexercise/controller/HospitalController.java
@@ -3,12 +3,14 @@ package com.spring.springjpaexercise.controller;
 import com.spring.springjpaexercise.domain.dto.HospitalResponse;
 import com.spring.springjpaexercise.domain.dto.ReviewCreateRequest;
 import com.spring.springjpaexercise.domain.dto.ReviewCreateResponse;
+import com.spring.springjpaexercise.domain.dto.ReviewResponse;
 import com.spring.springjpaexercise.service.HospitalService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/hospital")
@@ -23,7 +25,7 @@ public class HospitalController {
     @ApiOperation(value = "hospital 리스트 받기")
     @GetMapping("/list")
     public ResponseEntity<List<HospitalResponse>> listAll(){
-        List<HospitalResponse> hospitalList = hospitalService.find();
+        List<HospitalResponse> hospitalList = hospitalService.findAll();
         return ResponseEntity.ok().body(hospitalList);
     }
 
@@ -32,5 +34,11 @@ public class HospitalController {
     public ResponseEntity<ReviewCreateResponse> addReview(@PathVariable Long id, @RequestBody ReviewCreateRequest reviewCreateRequest){
         ReviewCreateResponse reviewCreateResponse = hospitalService.addReview(id,reviewCreateRequest);
         return ResponseEntity.ok().body(reviewCreateResponse);
+    }
+    @ApiOperation(value="병원 리뷰 보기")
+    @GetMapping("/{id}/review")
+    public ResponseEntity<List<ReviewResponse>> getReview(@PathVariable Long id){
+        List<ReviewResponse> reviewResponseList = hospitalService.findReview(id);
+        return ResponseEntity.ok().body(reviewResponseList);
     }
 }
