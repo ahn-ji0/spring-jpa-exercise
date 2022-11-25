7daa814aec24381c9f7d65232f482f705a88b942
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-25 10:32:31 +0900

    Add: Swagger 설명 추가

--- src/main/java/com/spring/springjpaexercise/controller/BookController.java
+++ src/main/java/com/spring/springjpaexercise/controller/BookController.java
@@ -5,6 +5,7 @@ import com.spring.springjpaexercise.domain.entity.Book;
 import com.spring.springjpaexercise.repository.AuthorRepository;
 import com.spring.springjpaexercise.repository.BookRepository;
 import com.spring.springjpaexercise.service.BookService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,7 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @ApiOperation(value = "Book 리스트 받기")
     @GetMapping("/list")
     public ResponseEntity<List<BookResponse>> listAll(){
         List<BookResponse> bookList = bookService.find();
